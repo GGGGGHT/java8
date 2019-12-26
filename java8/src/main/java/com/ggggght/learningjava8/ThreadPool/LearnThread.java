f7d66c078514a0b895be917af54a8523f360f111
ght <wz9712203617@gmail.com>
2019-12-26 20:22:46 +0800

    学习java.time中的API

---
+++ java8/src/main/java/com/ggggght/learningjava8/ThreadPool/LearnThread.java
@@ -0,0 +1,113 @@
+package com.ggggght.learningjava8.ThreadPool;
+
+import org.junit.Test;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ght
+ * @Desc
+ * @date 2019-12-26 10:32 AM
+ */
+
+@SuppressWarnings("all")
+public class LearnThread {
+
+    /**
+     * 问题：现有一个线程池，参数corePoolSize = 5，maximumPoolSize = 10，BlockingQueue阻塞队列长度为5，
+     * 此时有4个任务同时进来
+     * 1. 线程池会创建几条线程？
+     * 2. 如果4个任务还没处理完，这时又同时进来2个任务，问：线程池又会创建几条线程还是不会创建？
+     * 3. 如果前面6个任务还是没有处理完，这时又同时进来5个任务，问：线程池又会创建几条线程还是不会创建？
+     */
+    @Test
+    public void threadCreateTest() {
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(5,
+                10,
+                0L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(5),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+
+        for (int i = 0; i < 4; i++) {
+            pool.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        // 有4个任务同时进来 线程池会创建几条线程？
+        // 答: 4个任务同时进来 会创建4个线程来去执行任务
+        System.out.println("4 个任务同时进来时创建的线程数为: " + pool.getActiveCount());
+
+        for (int i = 0; i < 2; i++) {
+            pool.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        // 如果4个任务还没处理完，这时又同时进来2个任务，问：线程池又会创建几条线程还是不会创建？
+        // 答:线程池会创建1个线程去执行任务
+        System.out.println("4 个任务未执行完成,又进来2个任务后创建的线程数为: " + pool.getActiveCount());
+        System.out.println("此时队列的容量为: " + pool.getQueue().size());
+        // 如果前面6个任务还是没有处理完，这时又同时进来5个任务，问：线程池又会创建几条线程还是不会创建？
+        // 答:线程池会创建一个新的临时线程来执行
+        for (int i = 0; i < 5; i++) {
+            pool.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        System.out.println("又同时进来5个任务后创建的线程数量为: " + pool.getActiveCount());
+        System.out.println("此时队列的容量为: " + pool.getQueue().size());
+    }
+
+    @Test
+    public void test() {
+        List<Integer> nums = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 10000; i++) nums.add(1000000 + r.nextInt(1000000));
+
+        //System.out.println(nums);
+
+        Instant start = Instant.now();
+        nums.forEach(v -> isPrime(v));
+        Instant end = Instant.now();
+        System.out.println("expend time: " + Duration.between(start,end).getNano());
+
+        //使用parallel stream api
+
+        start = Instant.now();
+        nums.parallelStream().forEach(LearnThread::isPrime);
+        end = start = Instant.now();
+        System.out.println("expend time: " + Duration.between(start, end).getNano());
+
+    }
+
+    static boolean isPrime(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ java8/src/main/java/com/ggggght/learningjava8/Time/LearnTime.java
@@ -0,0 +1,209 @@
+package com.ggggght.learningjava8.Time;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * @author ght
+ * @Desc
+ * @date 2019-12-26 12:23 PM
+ * jdk8 之前用来处理时间的类或方法都不是线程安全的
+ * jdk8 之后的java.time包下的这些类都是线程安全的
+ * java.time.chrono
+ * java.time.format
+ * java.time.temporal
+ * java.time.zone
+ */
+
+@SuppressWarnings("all")
+public class LearnTime {
+    /**
+     * @target 不安全的时间格式化 会抛异常
+     */
+    @Test
+    public void timeFormatUnsafeTest() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+        Callable<Date> task = new Callable<Date>() {
+            @Override
+            public Date call() throws Exception {
+                return sdf.parse("20191226");
+            }
+        };
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        List<Future<Date>> results = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            results.add(pool.submit(task));
+        }
+
+        for (Future<Date> result : results) {
+            System.out.println(result.get());
+        }
+    }
+
+    /**
+     * @target 安全的时间格式化
+     */
+    @Test
+    public void timeFormatSafeTest() throws ExecutionException, InterruptedException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        Callable<LocalDate> task = new Callable<LocalDate>() {
+            @Override
+            public LocalDate call() throws Exception {
+                return LocalDate.parse("20191226", dtf);
+            }
+        };
+
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        List<Future<LocalDate>> results = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            results.add(pool.submit(task));
+        }
+
+        for (Future<LocalDate> result : results) {
+            System.out.println(result.get());
+        }
+    }
+
+    /**
+     * @target LocalDateTime测试
+     * LocalDateTime 与 LocalDate  LocalTime 用法一样 都是人类可读的形式
+     */
+    @Test
+    public void localDateTest() {
+        // 获取当前时间
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+        // 可以获取指定时间的对象
+        LocalDateTime ldt2 = LocalDateTime.of(2019, 12, 20, 10, 50, 40);
+        System.out.println(ldt2);
+
+        // 可以通过plusXXX()来加年/月/日/时/分/秒等 使用minusXXX()来减年/月/日/时/分/秒等
+        LocalDateTime ldt3 = ldt.plusYears(1L);
+        System.out.println(ldt3);
+        LocalDateTime ldt4 = ldt.minusYears(1l);
+        System.out.println(ldt4);
+
+        // 使用getXXX()或getXXXValue() 来获取相应的对象或值
+        Month month = ldt.getMonth();
+        System.out.println(month);
+        int monthValue = ldt.getMonthValue();
+        System.out.println(monthValue);
+
+    }
+
+    /**
+     * @target Instant测试
+     * Instant 时间戳(以 unix元年开始 从1970年1月1日 00:00:00秒开始 到目前为止的毫秒值)
+     */
+    @Test
+    public void instantTest() {
+        // 默认获取的是UTC时间
+        Instant utc = Instant.now();
+        System.out.println(utc);
+
+        // 获取当前的北京时间
+        OffsetDateTime cn = utc.atOffset(ZoneOffset.ofHours(8));
+        System.out.println(cn);
+
+        // 获取当前的时间戳
+        long timestamp = utc.toEpochMilli();
+        System.out.println(timestamp);
+    }
+
+    /**
+     * @target Duration 用来计算两个时间之间的间隔
+     */
+    @Test
+    public void durationTest() {
+        Instant start = Instant.now();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Instant end = Instant.now();
+        long seconds = Duration.between(start, end).getSeconds();
+        // 获取毫秒数是toMillis() 而不是getMillis()
+        long mills = Duration.between(start, end).toMillis();
+        int nano = Duration.between(start, end).getNano();
+        System.out.println("中间的间隔为: " + seconds + "秒");
+        System.out.println("中间的间隔为: " + mills + "毫秒");
+        System.out.println("中间的间隔为: " + nano + "纳秒");
+
+        LocalDateTime startTime = LocalDateTime.now();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LocalDateTime endTime = LocalDateTime.now();
+
+        long timeSecond = Duration.between(startTime, endTime).getSeconds();
+        long timeMills = Duration.between(startTime, endTime).toMillis();
+        long timeNano = Duration.between(startTime, endTime).getNano();
+        System.out.println("中间的间隔为: " + timeSecond + "秒");
+        System.out.println("中间的间隔为: " + timeMills + "毫秒");
+        System.out.println("中间的间隔为: " + timeNano + "纳秒");
+    }
+
+    /**
+     * @target Period   用来计算两个日期之间的间隔
+     */
+    @Test
+    public void periodTest() {
+        LocalDate date1 = LocalDate.of(2019, 12, 25);
+        LocalDate date2 = LocalDate.now();
+        Period period = Period.between(date1, date2);
+        int days = period.getDays();
+        int years = period.getYears();
+        int months = period.getMonths();
+        System.out.println(date1 + "与" + date2 + "的间隔的年数为" + years + "年");
+        System.out.println(date1 + "与" + date2 + "的间隔的月数为" + months + "月");
+        System.out.println(date1 + "与" + date2 + "的间隔的天数为" + days + "天");
+    }
+
+    /**
+     * @target TemporalAdjusters: 时间校正器
+     */
+    @Test
+    public void temporalAdjusterTest() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        // 获取下一个周日的时间
+        LocalDateTime nextSunDay = now.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+        System.out.println(nextSunDay);
+    }
+
+    /**
+     * @target DateTimeFormatter
+     */
+    @Test
+    public void dateTimeFormatterTest() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String stringNow = now.format(dtf);
+        String format = dtf.format(now);
+        System.out.println(stringNow);
+        System.out.println(format);
+        System.out.println();
+        LocalDateTime newDate = now.parse(format, dtf);
+        System.out.println(newDate);
+    }
+}
