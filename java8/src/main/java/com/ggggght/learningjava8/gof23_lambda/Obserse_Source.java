76e7134e73cf41ba3d51d0d28040d74314879c71
ggggght <wz9712203617@gmail.com>
2021-07-02 23:20:07 +0800

    feat: lambda

---
+++ java8/src/main/java/com/ggggght/learningjava8/gof23_lambda/Obserse_Source.java
@@ -0,0 +1,72 @@
+package com.ggggght.learningjava8.gof23_lambda;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@FunctionalInterface
+interface Observer {
+	void notify(String tweet);
+}
+
+interface Subject {
+	void registerObserver(Observer o);
+
+	void notifyObservers(String tweet);
+}
+
+/**
+ * 观察者模式
+ */
+public class Obserse_Source {
+	public static void main(String[] args) {
+		Feed feed = new Feed();
+		feed.registerObserver(new NYTimes());
+		feed.registerObserver(new Guardian());
+		feed.registerObserver(new LeMonde());
+
+		feed.notifyObservers("The queen said her favourite book is Java 8 in Action! ");
+	}
+}
+
+class Feed implements Subject {
+	private final List<Observer> observers = new ArrayList<>();
+
+	@Override
+	public void registerObserver(Observer o) {
+		this.observers.add(o);
+	}
+
+	@Override
+	public void notifyObservers(String tweet) {
+		this.observers.forEach(o -> o.notify(tweet));
+	}
+}
+
+class NYTimes implements Observer {
+	@Override
+	public void notify(String tweet) {
+		if (Objects.nonNull(tweet) && tweet.contains("money")) {
+			System.out.println("Breaking news in NY! " + tweet);
+		}
+	}
+}
+
+class Guardian implements Observer {
+	@Override
+	public void notify(String tweet) {
+		if (Objects.nonNull(tweet) && tweet.contains("queen")) {
+			System.out.println("Yet another news in London..." + tweet);
+		}
+	}
+}
+
+class LeMonde implements Observer {
+	@Override
+	public void notify(String tweet) {
+		if (Objects.nonNull(tweet) && tweet.contains("wine")) {
+			System.out.println("Today cheese ,wine and news! " + tweet);
+		}
+	}
+}
---
+++ java8/src/main/java/com/ggggght/learningjava8/gof23_lambda/Observe_lambda.java
@@ -0,0 +1,29 @@
+package com.ggggght.learningjava8.gof23_lambda;
+
+
+import java.util.Objects;
+
+public class Observe_lambda {
+	public static void main(String[] args) {
+		Feed feed = new Feed();
+		feed.registerObserver((tweet) -> {
+			if (Objects.nonNull(tweet) && tweet.contains("money")) {
+				System.out.println("Breaking news in NY! " + tweet);
+			}
+		});
+
+		feed.registerObserver((tweet) -> {
+			if (Objects.nonNull(tweet) && tweet.contains("queen")) {
+				System.out.println("Yet another news in London..." + tweet);
+			}
+		});
+
+		feed.registerObserver(tweet -> {
+			if (Objects.nonNull(tweet) && tweet.contains("wine")) {
+				System.out.println("Today cheese ,wine and news! " + tweet);
+			}
+		});
+
+		feed.notifyObservers("money");
+	}
+}
