ff08a6b0a1bc0e8cfa7c82a40717b412a4360b2f
GGGGGHT <wz9712203617@gmail.com>
2021-07-03 23:07:19 +0800

    feat: fork join

---
+++ java8/src/main/java/com/ggggght/learningjava8/ThreadPool/ForkJoinPoolTest.java
@@ -0,0 +1,19 @@
+package com.ggggght.learningjava8.threadPool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("all")
+public class ForkJoinPoolTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ForkJoinPoolTest.class);
+    
+    public static void main(String[] args) throws InterruptedException {
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.submit(() -> System.out.println("hello world"));
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.SECONDS);
+    }
+}
