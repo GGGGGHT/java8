d0409d34a7d1cfdc45d4b09aac633d023fb01b85
GGGGGHT <wz9712203617@gmail.com>
2021-07-11 22:06:14 +0800

    feat: state object

--- java8/src/test/java/com/ggggght/learningjava8/jmh/PSTest.java
+++ java8/src/test/java/com/ggggght/learningjava8/jmh/PSTest.java
@@ -1,9 +1,6 @@
 package com.ggggght.learningjava8.jmh;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -63,5 +60,35 @@ public class PSTest {
 	@OutputTimeUnit(TimeUnit.MICROSECONDS)
 	public void measureSamples() throws InterruptedException {
 		TimeUnit.MILLISECONDS.sleep(100);
+	}
+	
+	@Benchmark
+	public void measureUnshared(ThreadState state) {
+		// All benchmark threads will call in this method.
+		//
+		// However, since ThreadState is the Scope.Thread, each thread
+		// will have it's own copy of the state, and this benchmark
+		// will measure unshared case.
+		state.x++;
+	}
+	
+	@Benchmark
+	public void measureShared(BenchmarkState state) {
+		// All benchmark threads will call in this method.
+		//
+		// Since BenchmarkState is the Scope.Benchmark, all threads
+		// will share the state instance, and we will end up measuring
+		// shared case.
+		state.x++;
+	}
+	
+	@State(Scope.Thread)
+	public static class ThreadState {
+		volatile double x = Math.PI;
+	}
+	
+	@State(Scope.Benchmark)
+	public static class BenchmarkState {
+		volatile double x = Math.PI;
 	}
 }
