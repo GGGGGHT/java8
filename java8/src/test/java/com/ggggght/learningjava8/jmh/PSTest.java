34780cae071524f41559750e7d5428b22cdc8631
GGGGGHT <wz9712203617@gmail.com>
2021-07-11 18:06:51 +0800

    feat: measure Throughput

--- java8/src/test/java/com/ggggght/learningjava8/jmh/PSTest.java
+++ java8/src/test/java/com/ggggght/learningjava8/jmh/PSTest.java
@@ -1,13 +1,21 @@
 package com.ggggght.learningjava8.jmh;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;   
+import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 代码生成的位置  target/generated-sources/annotations/.../{Class_name}_{Method_name}.java
+ */
 @SuppressWarnings("all")
 public class PSTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PSTest.class);
@@ -20,5 +28,16 @@ public class PSTest {
 				.build();
 		
 		new Runner(opt).run();
+	}
+	
+	/**
+	 * 测试吞吐量
+	 * @throws InterruptedException
+	 */
+	@Benchmark
+	@BenchmarkMode(Mode.Throughput)
+	@OutputTimeUnit(TimeUnit.SECONDS)
+	public void measureThroughput() throws InterruptedException {
+		TimeUnit.MILLISECONDS.sleep(100);
 	}
 }
