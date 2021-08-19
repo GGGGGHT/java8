249780ccc33e858e809ecdae57ba6d4009255269
GGGGGHT <wz9712203617@gmail.com>
2021-08-19 08:58:45 +0800

    feat: Semaphore and Exchange

--- java8/src/main/java/com/ggggght/learningjava8/lock/LearnLock.java
+++ java8/src/main/java/com/ggggght/learningjava8/lock/LearnLock.java
@@ -1,45 +1,46 @@
 package com.ggggght.learningjava8.lock;
 
-
-
+import java.io.IOException;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
+import org.junit.Test;
 
 /**
  * @author ght
- * @Desc  学习锁的一些相关内容
+ * @Desc 学习锁的一些相关内容
  * @date 2019-12-27 10:42 AM
  */
 
 @SuppressWarnings("all")
 public class LearnLock {
 
-
-    public  static void lockSupportTest() {
-        Thread t = new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i);
-                if (5 == i) {
-                    LockSupport.park();
-                }
-
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.start();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-            LockSupport.unpark(t);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+  public static void lockSupportTest() {
+    Thread t = new Thread(() -> {
+      for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+        if (5 == i) {
+          LockSupport.park();
         }
-    }
 
-    public static void main(String[] args) {
+        try {
+          TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+    t.start();
+    try {
+      TimeUnit.SECONDS.sleep(5);
+      LockSupport.unpark(t);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
         /*Thread t = new Thread(()->{
             for (int i = 0; i < 10; i++) {
                 System.out.println(i);
@@ -64,5 +65,56 @@ public class LearnLock {
         t.start();
         //唤醒线程t
         LockSupport.unpark(t);*/
+  }
+
+  @Test
+  public void semaphoreTest() throws InterruptedException, IOException {
+    Semaphore semaphore = new Semaphore(3);
+
+    for (int i = 0; i < 5; i++) {
+      new Thread(() -> {
+        try {
+          semaphore.acquire();
+          Thread.sleep(1000);
+          System.out.println(
+              "Thread.currentThread().getName() = " + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }finally {
+          // 必须release 不然后续线程无法获得令牌
+          semaphore.release();
+        }
+      }).start();
     }
+  }
+
+  @Test
+  public void exchange() throws IOException {
+    Exchanger<Object> exchanger = new Exchanger<>();
+
+    new Thread(() -> {
+      String str = "T1";
+      Object res = null;
+      try {
+        res = exchanger.exchange(str);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      System.out.println(Thread.currentThread().getName() + " received: " + res);
+    }, "t1").start();
+
+
+    new Thread(() -> {
+      String str = "T2";
+      Object res = null;
+      try {
+        res = exchanger.exchange(str);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      System.out.println(Thread.currentThread().getName() + " received: " + res);
+    }, "t2").start();
+  }
 }
