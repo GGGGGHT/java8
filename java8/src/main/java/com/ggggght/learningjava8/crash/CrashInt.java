45c9c6cb6b32e375fcdc6f5dc4605d0c5f4f1dd8
GGGGGHT <wz9712203617@gmail.com>
2021-08-31 23:20:36 +0800

    feat: JVM crash

---
+++ java8/src/main/java/com/ggggght/learningjava8/crash/CrashInt.java
@@ -0,0 +1,31 @@
+package com.ggggght.learningjava8.crash;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+public class CrashInt {
+  int[] ia = new int[1];
+
+  final static Unsafe UNSAFE = getUnsafe();
+
+  public static Unsafe getUnsafe () {
+		try {
+			Field field = Unsafe.class.getDeclaredField("theUnsafe");
+			field.setAccessible(true);
+			return (Unsafe)field.get(null);
+		} catch (Exception ex) {
+			throw new RuntimeException("can't get Unsafe instance", ex);
+		}
+	}
+
+  public int crash() {
+    return ia.length;
+  }
+
+  public static void main(String[] args) {
+    CrashInt ci = new CrashInt();
+    UNSAFE.putLong(ci, 12L, 0xbadbabe);
+    ci.crash();
+  }
+}
