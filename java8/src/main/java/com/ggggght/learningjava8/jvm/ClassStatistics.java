10b2ff4f54cab6150c6ec736f2856be417e5e936
GGGGGHT <wz9712203617@gmail.com>
2021-09-01 22:41:44 +0800

    feat: JVM sa

---
+++ java8/src/main/java/com/ggggght/learningjava8/jvm/ClassStatistics.java
@@ -0,0 +1,37 @@
+package com.ggggght.learningjava8.jvm;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import sun.jvm.hotspot.memory.SystemDictionary;
+import sun.jvm.hotspot.oops.Klass;
+import sun.jvm.hotspot.oops.Oop;
+import sun.jvm.hotspot.oops.InstanceKlass;
+import sun.jvm.hotspot.runtime.VM;
+import sun.jvm.hotspot.tools.Tool;
+
+public class ClassStatistics extends Tool {
+
+  @Override
+  public void run() {
+
+    final SystemDictionary systemDictionary = VM.getVM().getSystemDictionary();
+    final ArrayList<InstanceKlass> instanceKlasses = new ArrayList<>();
+    final ArrayList<Oop> oops = new ArrayList<>();
+    systemDictionary.classesDo((klass, oop) -> {
+      if (klass instanceof InstanceKlass) {
+        instanceKlasses.add((InstanceKlass) klass);
+      }
+      oops.add(oop);
+    });
+
+    System.out.println("classes...");
+    instanceKlasses.stream().filter(Objects::nonNull).forEach(System.out::println);
+    System.out.println("-----------");
+    oops.stream().filter(Objects::nonNull).forEach(System.out::println);
+  }
+
+  public static void main(String[] args) {
+    ClassStatistics cs = new ClassStatistics();
+    cs.execute(args);
+  }
+}
