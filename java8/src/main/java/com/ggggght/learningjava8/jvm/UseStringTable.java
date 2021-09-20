fd4ca9582677f3f457be1f754547d9675294044a
GGGGGHT <wz9712203617@gmail.com>
2021-09-20 22:52:54 +0800

    feat: String table

---
+++ java8/src/main/java/com/ggggght/learningjava8/jvm/UseStringTable.java
@@ -0,0 +1,16 @@
+package com.ggggght.learningjava8.jvm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.jvm.hotspot.memory.StringTable;
+import sun.jvm.hotspot.runtime.VM;
+
+@SuppressWarnings("all")
+public class UseStringTable {
+  private static final Logger LOGGER = LoggerFactory.getLogger(UseStringTable.class);
+
+    public static void main(String[] args) {
+        final StringTable stringTable = VM.getVM().getStringTable();
+        System.out.println(stringTable.getTheTable());
+    }
+}
