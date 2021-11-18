cc382ee92f7725bd3c0d701e31a1f2c38851e9a6
ggggght <wz9712203617@gmail.com>
2021-11-18 15:07:41 +0800

    style: reformat code

--- java8/src/main/java/com/ggggght/learningjava8/threadPool/LearnThread.java
+++ java8/src/main/java/com/ggggght/learningjava8/threadPool/LearnThread.java
@@ -16,47 +16,39 @@ import java.util.concurrent.*;
  * @Desc
  * @date 2019-12-26 10:32 AM
  * <p>
- * ThreadPoolExecutor的执行流程
- * 1. 提交任务
- * 2. 首先判断工作线程是否小于核心线程数 如果小于核心线程数 直接进行addWorker()
- * 在addWorker方法中会判断线程池的运行状态，如果线程池已停止运行则返回false
- * 3. 如果当前线程数已大于核心线程数 则需要判断阻塞队列是否已满 如果队列未满 则将任务放入阻塞队列中 再次判断线程池是否是运行状态
- * 如果线程池已停止运行，则将放入队列的任务移除，并拒绝该任务
- * 4. 再次调用addWorker方法 使用非核心线程去执行任务 如果添加失败 则调用reject 拒绝该任务
+ * ThreadPoolExecutor的执行流程 1. 提交任务 2. 首先判断工作线程是否小于核心线程数 如果小于核心线程数 直接进行addWorker()
+ * 在addWorker方法中会判断线程池的运行状态，如果线程池已停止运行则返回false 3. 如果当前线程数已大于核心线程数 则需要判断阻塞队列是否已满 如果队列未满
+ * 则将任务放入阻塞队列中 再次判断线程池是否是运行状态 如果线程池已停止运行，则将放入队列的任务移除，并拒绝该任务 4. 再次调用addWorker方法
+ * 使用非核心线程去执行任务 如果添加失败 则调用reject 拒绝该任务
  */
 
 @SuppressWarnings("all")
 public class LearnThread {
+
 	private static final int COREPOOLSIZE = 10;
+
 	private static final int MAXPOOLSIZE = 10;
+
 	private static final long KEEPALIVETIME = 0L;
+
 	private static final BlockingQueue WORKINGQUEUE = new LinkedBlockingDeque();
 
 	/**
 	 * 问题：现有一个线程池，参数corePoolSize = 5，maximumPoolSize = 10，BlockingQueue阻塞队列长度为5，
-	 * 此时有4个任务同时进来
-	 * 1. 线程池会创建几条线程？
-	 * 2. 如果4个任务还没处理完，这时又同时进来2个任务，问：线程池又会创建几条线程还是不会创建？
-	 * 3. 如果前面6个任务还是没有处理完，这时又同时进来5个任务，问：线程池又会创建几条线程还是不会创建？
+	 * 此时有4个任务同时进来 1. 线程池会创建几条线程？ 2. 如果4个任务还没处理完，这时又同时进来2个任务，问：线程池又会创建几条线程还是不会创建？ 3.
+	 * 如果前面6个任务还是没有处理完，这时又同时进来5个任务，问：线程池又会创建几条线程还是不会创建？
 	 */
 	@Test
 	public void threadCreateTest() {
-		ThreadPoolExecutor pool = new ThreadPoolExecutor(
-				5,
-				10,
-				0L,
-				TimeUnit.SECONDS,
-				new LinkedBlockingDeque<>(5),
-				Executors.defaultThreadFactory(),
-				new ThreadPoolExecutor.CallerRunsPolicy()
-		);
-
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 10, 0L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(5),
+				Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
 
 		for (int i = 0; i < 4; i++) {
 			pool.execute(() -> {
 				try {
 					TimeUnit.SECONDS.sleep(5);
-				} catch (Exception e) {
+				}
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			});
@@ -69,7 +61,8 @@ public class LearnThread {
 			pool.execute(() -> {
 				try {
 					TimeUnit.SECONDS.sleep(5);
-				} catch (Exception e) {
+				}
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			});
@@ -84,7 +77,8 @@ public class LearnThread {
 			pool.execute(() -> {
 				try {
 					TimeUnit.SECONDS.sleep(5);
-				} catch (Exception e) {
+				}
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			});
@@ -97,16 +91,17 @@ public class LearnThread {
 	public void test() {
 		List<Integer> nums = new ArrayList<>();
 		Random r = new Random();
-		for (int i = 0; i < 10000; i++) nums.add(1000000 + r.nextInt(1000000));
+		for (int i = 0; i < 10000; i++)
+			nums.add(1000000 + r.nextInt(1000000));
 
-		//System.out.println(nums);
+		// System.out.println(nums);
 
 		Instant start = Instant.now();
 		nums.forEach(v -> isPrime(v));
 		Instant end = Instant.now();
 		System.out.println("expend time: " + Duration.between(start, end).getNano());
 
-		//使用parallel stream api
+		// 使用parallel stream api
 
 		start = Instant.now();
 		nums.parallelStream().forEach(LearnThread::isPrime);
@@ -117,20 +112,16 @@ public class LearnThread {
 
 	static boolean isPrime(int num) {
 		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) return false;
+			if (num % i == 0)
+				return false;
 		}
 		return true;
 	}
 
 	@Test
 	public void test1() {
-		ThreadPoolExecutor pool = new ThreadPoolExecutor(
-				COREPOOLSIZE,
-				MAXPOOLSIZE,
-				KEEPALIVETIME,
-				TimeUnit.SECONDS,
-				WORKINGQUEUE
-		);
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(COREPOOLSIZE, MAXPOOLSIZE, KEEPALIVETIME, TimeUnit.SECONDS,
+				WORKINGQUEUE);
 	}
 
 	/**
@@ -144,13 +135,13 @@ public class LearnThread {
 		executor.execute(() -> {
 			try {
 				executor.shutdown();
-			} catch (Exception e) {
+			}
+			catch (Exception e) {
 				e.printStackTrace();
 			}
 
 			System.out.println(" Thread.currentThread().getName() = " + Thread.currentThread().getName() + " 执行完成");
 		});
-
 
 		Thread.sleep(500);
 		executor.execute(() -> {
@@ -159,9 +150,11 @@ public class LearnThread {
 
 		executor.awaitTermination(3, TimeUnit.SECONDS);
 	}
+
 }
 
 class TestCallable {
+
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		final ThreadDemo threadDemo = new ThreadDemo();
 		FutureTask<Integer> futureTask = new FutureTask<Integer>(threadDemo);
@@ -169,9 +162,11 @@ class TestCallable {
 		final Optional<Integer> value = Optional.ofNullable(futureTask.get());
 		value.ifPresent(System.out::print);
 	}
+
 }
 
 class ThreadDemo implements Callable<Integer> {
+
 	@Override
 	public Integer call() throws Exception {
 		int sum = 0;
@@ -182,22 +177,20 @@ class ThreadDemo implements Callable<Integer> {
 	}
 
 	public void catchException() throws InterruptedException {
-		ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.HOURS,
-		                                                         new LinkedBlockingDeque<>(),
-		                                                         new ThreadFactory() {
-			                                                         @Override
-			                                                         public Thread newThread(@NotNull Runnable r) {
-				                                                         Thread thread = new Thread(r);
-				                                                         thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-					                                                         @Override
-					                                                         public void uncaughtException(Thread t, Throwable e) {
-						                                                         System.out.println(e);
-					                                                         }
-				                                                         });
-				                                                         return thread;
-			                                                         }
-		                                                         }
-		);
+		ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.HOURS, new LinkedBlockingDeque<>(),
+				new ThreadFactory() {
+					@Override
+					public Thread newThread(@NotNull Runnable r) {
+						Thread thread = new Thread(r);
+						thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+							@Override
+							public void uncaughtException(Thread t, Throwable e) {
+								System.out.println(e);
+							}
+						});
+						return thread;
+					}
+				});
 
 		poolExecutor.execute(() -> {
 			int i = 1 / 0;
@@ -206,4 +199,5 @@ class ThreadDemo implements Callable<Integer> {
 		poolExecutor.shutdown();
 		poolExecutor.awaitTermination(1L, TimeUnit.MINUTES);
 	}
+
 }
--- java8/src/main/java/com/ggggght/learningjava8/threadPool/SynchorizedTest.java
+++ java8/src/main/java/com/ggggght/learningjava8/threadPool/SynchorizedTest.java
@@ -1,61 +1,43 @@
-package com.ggggght.learningjava8.threadPool;
-
+package com.ggggght.learningjava8.ThreadPool;
 
 /**
  * @author ght
  * @Desc 学习synchronized关键字的使用
- * @date 2020-04-14 11:23
- * synchronized的用法
- * 1. 普通同步方法,锁的是当前实例对象
- * 2. 静态同步方法,锁的是当前类的class对象
- * 3. 同步方法块,锁的是括号里的对象
+ * @date 2020-04-14 11:23 synchronized的用法 1. 普通同步方法,锁的是当前实例对象 2. 静态同步方法,锁的是当前类的class对象 3.
+ * 同步方法块,锁的是括号里的对象
  *
- * 对于同步方法,JVM采用ACC_SYNCHRONIZED标志符来实现同步
- * 对于同步代码块,采用monitorenter和monitorexit来实现同步
+ * 对于同步方法,JVM采用ACC_SYNCHRONIZED标志符来实现同步 对于同步代码块,采用monitorenter和monitorexit来实现同步
  *
- * 总结:
- *  并发编程中通过同步互斥访问临界资源来解决线程安全问题，Java中常用synchronized标记同步块达到加锁的目的。
- *  synchronized用法有两种，修饰方法和修饰同步代码块。
- *  synchronized的实现原理：每一个Java对象都会关联一个Monitor，通过Monitor对线程的操作实现synchronized对象锁。
- *  并发编程中synchronized可以保证原子性、可见性、有序性。
+ * 总结: 并发编程中通过同步互斥访问临界资源来解决线程安全问题，Java中常用synchronized标记同步块达到加锁的目的。
+ * synchronized用法有两种，修饰方法和修饰同步代码块。
+ * synchronized的实现原理：每一个Java对象都会关联一个Monitor，通过Monitor对线程的操作实现synchronized对象锁。
+ * 并发编程中synchronized可以保证原子性、可见性、有序性。
  *
  * ---------------------------------------------------------------------------------------------------
- * synchronzied的优化:
- * 问题: 为什么需要对synchronzied优化?
- * 答: 因为java的线程是映射到操作系统的线程之上的,所以对一个线程的挂起或者是唤醒 都需要从用户态映射到内核态.状态转换需要花费很多时间.
- * 所以当频繁的通过sync去实现同步会严重的影响到程序的执行效率.因此jdk对sync进行了种种优化
+ * synchronzied的优化: 问题: 为什么需要对synchronzied优化? 答: 因为java的线程是映射到操作系统的线程之上的,所以对一个线程的挂起或者是唤醒
+ * 都需要从用户态映射到内核态.状态转换需要花费很多时间. 所以当频繁的通过sync去实现同步会严重的影响到程序的执行效率.因此jdk对sync进行了种种优化
  *
- * 自旋锁和适应自旋锁:
- * 多数情况下.线程持有锁的时间都不会很长.为了这一段很短的时间频繁的阻塞或唤醒线程是非常不值得的,所以引入了自旋锁.
+ * 自旋锁和适应自旋锁: 多数情况下.线程持有锁的时间都不会很长.为了这一段很短的时间频繁的阻塞或唤醒线程是非常不值得的,所以引入了自旋锁.
  *
- * 自旋锁:
- * 当锁被占用时，当前想要获取锁的线程不会被立即挂起，而是做几个空循环，看持有锁的线程是否会很快释放锁。
+ * 自旋锁: 当锁被占用时，当前想要获取锁的线程不会被立即挂起，而是做几个空循环，看持有锁的线程是否会很快释放锁。
  * 在经过若干次循环后，如果得到锁，就顺利进入临界区；如果还不能获得锁，那就会将线程在操作系统层面挂起。
  *
- * 自旋锁和阻塞的区别:
- * 主要区别在于:是否放弃CPU的执行时间.
+ * 自旋锁和阻塞的区别: 主要区别在于:是否放弃CPU的执行时间.
  * 阻塞放弃了CPU时间，进入了等待区，等待被唤醒。响应慢。自旋锁一直占用CPU时间，时刻检查共享资源是否可以被访问，所以响应速度更快。
  * 如果持有锁的线程很快就释放了锁,那么自旋的效率就非常高.但如果持有锁的线程占用锁的时间较长,等待锁的线程自旋一定次数还是拿不到锁而被阻塞,
  * 那么自旋就白的浪费了CPU的资源.所以自旋的次数直接决定了自旋锁的性能.JDK自旋的默认次数为10次,可以通过参数 -XX:PreBlockSpin来进行调整
  *
- * 自适应自旋锁:
- * 自适应自旋锁指的是自旋的次数不再是固定的.它是由前一次在同一个锁上的自旋时间及锁的拥有者的状态来决定.线程如果自旋成功了.那么下次自旋的
+ * 自适应自旋锁: 自适应自旋锁指的是自旋的次数不再是固定的.它是由前一次在同一个锁上的自旋时间及锁的拥有者的状态来决定.线程如果自旋成功了.那么下次自旋的
  * 次数会更加多.因为虚拟机认为既然上次成功了.那么此次自旋也很有可能会再次成功.那么它就会允许自旋等待持续的次数更多.
  * 如果对于某个锁,很少有自旋能够成功的.那么在以后要获取这个锁的时候自旋的次数会减少甚至活省略掉自旋过程,以免浪费处理器资源.
  *
- * 锁消除:
- * 如果JVM检测到某段代码不可能存在共享竞争,JVM会对这段代码的同步锁进行锁消除.
+ * 锁消除: 如果JVM检测到某段代码不可能存在共享竞争,JVM会对这段代码的同步锁进行锁消除.
  *
  * ----------------------------------------------------------------------------------------------------------------
- * synchronzied锁一共有四种状态:
- * - 无锁
- * - 偏向锁
- * - 轻量级锁
- * - 重量级锁
+ * synchronzied锁一共有四种状态: - 无锁 - 偏向锁 - 轻量级锁 - 重量级锁
  *
  * 偏向锁: 多数情况下锁不仅不存在多线程竞争,而且总是由同一个线程多次获取,所以引入偏向锁让线程获得锁的代价更低.偏向锁认为环境中不存在
- * 竞争情况，锁只被一个线程持有，一旦有不同的线程获取或竞争锁对象，偏向锁就升级为轻量级锁。偏向锁在无多线程竞争的情况下可以减少不必须
- * 要的轻量级锁执行路径。
+ * 竞争情况，锁只被一个线程持有，一旦有不同的线程获取或竞争锁对象，偏向锁就升级为轻量级锁。偏向锁在无多线程竞争的情况下可以减少不必须 要的轻量级锁执行路径。
  *
  * 轻量级锁: 在大多数情况下同步块并不会出现竞争情况，大部分情况是不同线程交替持有锁，所以引入轻量级锁可以减少重量级锁对线程的阻塞带来的开销。
  * 轻量级锁认为环境中线程几乎没有对锁对象的竞争，即使有竞争也只需要稍微等待（自旋）下就可以获取锁，但是自旋次数有限制，如果超过该次数，则会升级为重量级锁。
@@ -65,36 +47,38 @@ package com.ggggght.learningjava8.threadPool;
 
 @SuppressWarnings("all")
 public class SynchorizedTest {
-    private static int count;
 
-    // 1.实例方法
-    public synchronized void lockMethod(int value){
-        count += value;
-    }
+	private static int count;
 
-    // 2.实例方法中的同步块 (等价于1)
-    public void lockBlock(int value){
-        synchronized(this){
-            count += value;
-        }
-    }
+	// 1.实例方法
+	public synchronized void lockMethod(int value) {
+		count += value;
+	}
 
-    // 3.静态方法
-    public static synchronized void staticLockMethod(int value){
-        count += value;
-    }
+	// 2.实例方法中的同步块 (等价于1)
+	public void lockBlock(int value) {
+		synchronized (this) {
+			count += value;
+		}
+	}
 
-    // 4.静态方法中的同步块 (等价于3)
-    public static void staticLockBlock(int value){
-        // 问题: 当在静态方法中使用同步代码块时synchronized(SynchorizedTest.class)可否替换为synchronized(this)
-        // 答: 不可以 因为在静态方法可以不通过对象来调用.而synchroized锁的必须是对象 所以此处不可以替换为this
-        // 问题: SynchorizedTest这个类是否是单例的
-        // 答: 如果是在同一个ClassLoader空间那它一定是单例.不是同一个类加载器就不是了，不同的类加载器互相之间也不能访问。
-        synchronized(SynchorizedTest.class){
-            count += value;
-        }
-    }
+	// 3.静态方法
+	public static synchronized void staticLockMethod(int value) {
+		count += value;
+	}
 
-    public static void main(String[] args) {
-    }
+	// 4.静态方法中的同步块 (等价于3)
+	public static void staticLockBlock(int value) {
+		// 问题: 当在静态方法中使用同步代码块时synchronized(SynchorizedTest.class)可否替换为synchronized(this)
+		// 答: 不可以 因为在静态方法可以不通过对象来调用.而synchroized锁的必须是对象 所以此处不可以替换为this
+		// 问题: SynchorizedTest这个类是否是单例的
+		// 答: 如果是在同一个ClassLoader空间那它一定是单例.不是同一个类加载器就不是了，不同的类加载器互相之间也不能访问。
+		synchronized (SynchorizedTest.class) {
+			count += value;
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
 }
