4759ae748663dda4ee29996ea636429e6e8cb70f
ggggght <wz9712203617@gmail.com>
2021-11-18 15:00:12 +0800

    fix: update CI

--- java8/src/main/java/com/ggggght/learningjava8/Learningjava8Application.java
+++ java8/src/main/java/com/ggggght/learningjava8/Learningjava8Application.java
@@ -35,13 +35,13 @@ public class Learningjava8Application implements ApplicationRunner {
 		// MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 		
 		// Construct the ObjectName for the Hello MBean we will register
-		ObjectName mbeanName = new ObjectName("com.example:type=Hello");
+		// ObjectName mbeanName = new ObjectName("com.example:type=Hello");
 		
 		// Create the Hello World MBean
-		Hello mbean = new Hello();
+		// Hello mbean = new Hello();
 		
 		// Register the Hello World MBean
-		mbs.registerMBean(mbean, mbeanName);
+		// mbs.registerMBean(mbean, mbeanName);
 		
 		// MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 		// mbs.registerMBean(new Hello(),new ObjectName( "com.ggggght:type=Hello"));
