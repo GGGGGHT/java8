7bf747f39231ca2cc7f6204911b2358e296363a2
ggggght <wz9712203617@gmail.com>
2021-11-23 17:34:36 +0800

    feat: spliterator

--- java8/src/main/java/com/ggggght/learningjava8/ThreadPool/MySpliterator.java
+++ java8/src/main/java/com/ggggght/learningjava8/ThreadPool/MySpliterator.java
@@ -1,5 +1,7 @@
 package com.ggggght.learningjava8.ThreadPool;
 
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import javax.management.ServiceNotFoundException;
@@ -15,7 +17,9 @@ public class MySpliterator {
       if (Character.isWhitespace(c)) {
         lastSpace = true;
       } else {
-        if (lastSpace) counter++;
+        if (lastSpace) {
+          counter++;
+        }
         lastSpace = false;
       }
     }
@@ -29,5 +33,41 @@ public class MySpliterator {
     System.out.println("countWordsIteratively(SENTENCE) = " + countWordsIteratively(SENTENCE));
     Stream<Character> stream =
         IntStream.range(0, SENTENCE.length()).mapToObj(SENTENCE::charAt);
+  }
+}
+
+class WordCounterSpliterator implements Spliterator<Character> {
+  private final String str;
+  private int currentChar = 0;
+
+  public WordCounterSpliterator(String str) {
+    this.str = str;
+  }
+
+  @Override public boolean tryAdvance(Consumer<? super Character> action) {
+    action.accept(str.charAt(currentChar++));
+    return currentChar < str.length();
+  }
+
+  @Override public Spliterator<Character> trySplit() {
+    int currentSize = str.length() - currentChar;
+    if (currentSize < 10) return null;
+
+    for (int splitPos = currentSize / 2 + currentChar; splitPos < str.length(); splitPos++) {
+      WordCounterSpliterator spliterator =
+          new WordCounterSpliterator(str.substring(currentSize, splitPos));
+
+      currentChar = splitPos;
+      return spliterator;
+    }
+    return null;
+  }
+
+  @Override public long estimateSize() {
+    return str.length() - currentChar;
+  }
+
+  @Override public int characteristics() {
+    return ORDERED + SIZED + SUBSIZED + NONNULL + IMMUTABLE;
   }
 }
