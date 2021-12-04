ece7e8cc2d29ff18a29ba7916c242fbfe6369c27
ggggght <wz9712203617@gmail.com>
2021-12-04 22:59:07 +0800

    feat: 重排链表(一次通过)
    https://leetcode-cn.com/problems/reorder-list/
    
    什么优化,复杂度.
    在老夫面前没有优化! 算法就是枚举,数据结构就是链表. 全给它串起来,存取就遍历O(n^2),O(n^3),O(2^n).
    动态规划？回溯剪枝?不存在的!又不是不能跑! 复制粘贴, 能跑就行.搞那花里胡哨的干啥.

---
+++ java8/src/main/java/com/ggggght/learningjava8/leetcode/Leetcode143.java
@@ -0,0 +1,89 @@
+package com.ggggght.learningjava8.leetcode;
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+
+  @Override public String toString() {
+    return "ListNode{" +
+        "val=" + val +
+        ", next=" + next +
+        '}';
+  }
+}
+
+public class Leetcode1 {
+  public static void main(String[] args) {
+    Leetcode1 solution = new Leetcode1();
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    // head.next.next.next.next = new ListNode(5);
+    // head.next.next.next.next.next = new ListNode(2);
+    solution.reorderList(head);
+  }
+
+  public void reorderList(ListNode head) {
+    var len = 1;
+    var tail = head;
+    while (tail.next != null) {
+      tail = tail.next;
+      len++;
+    }
+
+    // tail指向最后的一个节点
+    var mid = ((len & 1) == 0) ? len >> 1 : (len >> 1) + 1;
+    var tmp = head;
+    for (int i = 1; i < mid; i++) {
+      tmp = tmp.next;
+    }
+    var midP = tmp;
+    tmp = tmp.next;
+    // 从tmp之后翻转链表
+    ListNode newNode = null;
+    while (tmp != null) {
+      var next = tmp.next;
+      tmp.next = newNode;
+      newNode = tmp;
+      tmp = next;
+    }
+
+    midP.next = newNode;
+
+    var s = head;
+    while (head != midP) {
+      // 下一个要插入的位置
+      var nextInsertPosition = head.next;
+      var needAdd = midP.next;
+      midP.next = midP.next.next;
+      needAdd.next = nextInsertPosition;
+      head.next = needAdd;
+      head = nextInsertPosition;
+    }
+  }
+}
+
