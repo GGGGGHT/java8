febe835b238c6b4500a006a2cc4c61fbea4dfd72
ggggght <wz9712203617@gmail.com>
2021-12-21 16:15:32 +0800

    feat: 43. 字符串相乘 (优化竖式)
    https://leetcode-cn.com/problems/multiply-strings/

--- java8/src/main/java/com/ggggght/learningjava8/leetcode/Leetcode43.java
+++ java8/src/main/java/com/ggggght/learningjava8/leetcode/Leetcode43.java
@@ -1,5 +1,7 @@
 package com.ggggght.learningjava8.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <p>给定两个以字符串形式表示的非负整数&nbsp;<code>num1</code>&nbsp;和&nbsp;<code>num2</code>，返回&nbsp;<code>num1</code>&nbsp;和&nbsp;<code>num2</code>&nbsp;的乘积，它们的乘积也表示为字符串形式。</p>
  *
@@ -29,8 +31,9 @@ public class Leetcode43 {
     // System.out.println("multiply(\"2\" ,\"3\") = " + multiply("2", "3"));
     // System.out.println("multiply(\"123\",\"456\") = " + multiply("123", "456"));
     System.out.println("multiply(\"123\",\"456\") = " + multiply1("498828660196", "840477629533"));
-    System.out.println("multiply(\"123\",\"456\") = " + multiply("498828660196", "840477629533"));
     // System.out.println("multiply(\"123\",\"456\") = " + multiply("123456789", "987654321"));
+    System.out.println("multiply2(\"123\",\"456\") = " + multiply2("123", "456"));
+  System.out.println("multiply(\"123\",\"456\") = " + multiply2("498828660196", "840477629533"));
   }
 
   public static String multiply1(String num1, String num2) {
@@ -95,14 +98,14 @@ public class Leetcode43 {
       int j = num2.charAt(i) - '0';
       // 使j与num1中每个数相乘
       int carry = 0;
-      for (int a = num1.length() - 1; a >= 0 ; a--) {
+      for (int a = num1.length() - 1; a >= 0; a--) {
         int t = num1.charAt(a) - '0';
         int p = (j * t + carry) % 10;
         carry = (j * t + carry) / 10;
         builder.append(p);
       }
 
-      if(carry != 0) builder.append(carry);
+      if (carry != 0) builder.append(carry);
       res = addString(builder.reverse().toString(), res);
     }
 
@@ -123,5 +126,29 @@ public class Leetcode43 {
     }
 
     return builder.reverse().toString();
+  }
+
+  public static String multiply2(String num1, String num2) {
+    if(num1.equals("0") || num2.equals("0")) return "0";
+    var len1 = num1.length();
+    var len2 = num2.length();
+    var arr = new int[len1 + len2];
+    for (int i = len1 - 1; i >= 0; i--) {
+      int i1 = num1.charAt(i) - '0';
+      for (int j = len2 - 1; j >= 0; j--) {
+        int j1 = num2.charAt(j) - '0';
+        int p = i1 * j1 + arr[i + j + 1];
+        // 是否需要进位
+        arr[i + j] += p / 10;
+        arr[i + j + 1] = p % 10;
+      }
+    }
+
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < arr.length; i++) {
+      if(i == 0 && arr[i] == 0) continue;
+      builder.append(arr[i]);
+    }
+    return builder.toString();
   }
 }
