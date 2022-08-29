54c53879d10f30581750d2295bc9c1f16905d559
GGGGGHT <wz9712203617@gmail.com>
2022-08-29 21:28:37 +0800

    Update JMXUtil.java

--- java8/src/main/java/com/ggggght/learningjava8/util/JMXUtil.java
+++ java8/src/main/java/com/ggggght/learningjava8/util/JMXUtil.java
@@ -16,7 +16,7 @@ public class JMXUtil {
     /**
      * get JMX connect URL
      * @param id target vm pid
-     * @return url eg: service:jmx:rmi://127.0.0.1/stub/rO0ABXNyAC5qYXZheC5tYW5hZ2VtZW50LnJlbW90ZS5ybWkuUk1JU2VydmVySW1wbF9TdHViAAAAAAAAAAICAAB4cgAaamF2YS5ybWkuc2VydmVyLlJlbW90ZVN0dWLp/tzJi+FlGgIAAHhyABxqYXZhLnJtaS5zZXJ2ZXIuUmVtb3RlT2JqZWN002G0kQxhMx4DAAB4cHc0AAtVbmljYXN0UmVmMgAACTEyNy4wLjAuMQAAMb2ZGQ1IqvpWrdJNZeoAAAGC4md/z4ABAHg=
+     * @return url 
      * @throws IOException 
      * @throws AgentLoadException
      * @throws AgentInitializationException
